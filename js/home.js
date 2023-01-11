29a2f0b74c48fc6e3744a4e5e4002ed3f412865f
MustaphaBousilDev <bousilmustapha@gmail.com>
2023-01-11 15:17:19 +0100

    finish

---
+++ js/home.js
@@ -0,0 +1,6 @@
+document.querySelector('.menu-bar').addEventListener('click',()=>{
+    document.querySelector('.links-ul-mobile').classList.toggle('actives')
+    document.querySelector('.header__app').classList.toggle('actives')
+})
+
+ 
--- main.js
+++
@@ -1,6 +0,0 @@
-document.querySelector('.menu-bar').addEventListener('click',()=>{
-    document.querySelector('.links-ul-mobile').classList.toggle('actives')
-    document.querySelector('.header__app').classList.toggle('actives')
-})
-
- 
